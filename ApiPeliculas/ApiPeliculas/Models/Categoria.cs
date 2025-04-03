954797f3664d8b79635437fbc316cf5655885527
Adrian Castro Beiro <adrian.castrobeiro@plexus.tech>
2025-04-03 16:19:36 +0200

    crud hecho, empezando apiRest

---
+++ ApiPeliculas/ApiPeliculas/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Fecha de Creación")]
+        public DateTime FechaCreacion { get; set; }
+}
+}
---
+++ ApiPeliculas/ApiPeliculas/Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using ApiPeliculas.Models;
+
+namespace ApiPeliculas.Repositorio.IRepositorio
+{
+    public interface ICategoriaRepositorio
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int CategoriaId);
+        bool ExisteCategoria(int Id);
+        bool ExisteCategoria(string nombre);
+
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool EliminarCategoria(Categoria categoria);
+        bool Guardar();
+
+
+    }
+}
