f24144e02eafda390bb7397584aed87956b14c62
Adrian Castro Beiro <adrian.castrobeiro@plexus.tech>
2025-04-04 08:53:20 +0200

    autoMapper Peliculas

---
+++ ApiPeliculas/ApiPeliculas/PeliculasMappers/PeliculasMapper.cs
@@ -0,0 +1,17 @@
+﻿using ApiPeliculas.Modelos.Dtos;
+using ApiPeliculas.Models;
+using ApiPeliculas.Models.Dtos;
+using AutoMapper;
+
+namespace ApiPeliculas.PeliculasMapper
+{
+    public class PeliculasMapper :Profile
+    {
+        public PeliculasMapper()
+        {
+            CreateMap<Categoria, CategoriaDto>().ReverseMap();
+            CreateMap<Categoria, CrearCategoriaDto>().ReverseMap();
+
+        }
+    }
+}
