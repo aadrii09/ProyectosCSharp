fbc24dd303a1a4ed8d0babd8800d56fc80ca4175
Adrian Castro Beiro <adrian.castrobeiro@plexus.tech>
2025-04-07 08:56:04 +0200

    empezando con el controller de peliculas

--- ApiPeliculas/ApiPeliculas/Controllers/PeliculasController.cs
+++ ApiPeliculas/ApiPeliculas/Controllers/PeliculasController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiPeliculas.Repositorio.IRepositorio;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiPeliculas.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/peliculas")]
     [ApiController]
     public class PeliculasController : ControllerBase
     {
+            private readonly IPeliculaRepositorio _peliculaRepositorio;
+            private readonly IMapper _mapper;
+
+            public PeliculasController(IPeliculaRepositorio peliculaRepo, IMapper mapper)
+            {
+                _peliculaRepositorio = peliculaRepo;
+                _mapper = mapper;
+            }
     }
 }
